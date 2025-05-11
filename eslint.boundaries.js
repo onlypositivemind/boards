401b72ab4946f6d4862df0c84cca518a104c80f7
Evgeny Petrov <evgenypetrov5230@gmail.com>
2025-05-11 12:38:15 +0300

    init

---
+++ eslint.boundaries.js
@@ -0,0 +1,69 @@
+import boundaries from 'eslint-plugin-boundaries'
+
+export const eslintBoundariesConfig = {
+  plugins: {
+    boundaries,
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
+
+    'boundaries/elements': [
+      {
+        type: 'app',
+        pattern: './src/app',
+      },
+      {
+        type: 'features',
+        pattern: './src/features/*',
+      },
+      {
+        type: 'shared',
+        pattern: './src/shared',
+      },
+    ],
+  },
+  rules: {
+    'boundaries/element-types': [
+      2,
+      {
+        default: 'allow',
+        rules: [
+          {
+            from: 'shared',
+            disallow: ['app', 'features'],
+            message:
+              'The module of the underlying layer (${file.type}) cannot import the module of the overlying layer (${dependency.type}).',
+          },
+          {
+            from: 'features',
+            disallow: ['app'],
+            message:
+              'The module of the underlying layer (${file.type}) cannot import the module of the overlying layer (${dependency.type}).',
+          },
+        ],
+      },
+    ],
+    'boundaries/entry-point': [
+      2,
+      {
+        default: 'disallow',
+        message:
+          'The module (${file.type}) must use the public API. Direct import from ${dependency.source} is prohibited.',
+        rules: [
+          {
+            target: ['shared', 'app'],
+            allow: '**',
+          },
+          {
+            target: ['features'],
+            allow: 'index.(ts|tsx)',
+          },
+        ],
+      },
+    ],
+  },
+}
